9c4225af942a77457f0ffb35946f9eaa598d7b78
Johannes Honsel <jhonsel33@gmail.com>
2020-05-26 12:46:45 +0200

    first Upload

---
+++ renamePHPFiles.py
@@ -0,0 +1,11 @@
+import json
+import os
+
+with open('/Users/Jojo/Desktop/downloadsForum') as json_file:
+    data = json.load(json_file)
+
+for file in data:
+    try:
+        os.rename(str(file['real_file']), str(file['file_name']))
+    except Exception as e:
+        print(e)
